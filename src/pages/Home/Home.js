98e2d30e68747aacb44dc75c5ca9a76742306d62
Sazzad Hossain <piash@Pishu.local>
2023-02-17 20:11:03 +0600

    mac upload

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import bgImg from './../../assets/bg.webp';
-import { FaRegHeart } from 'react-icons/fa'
-import { GoLocation } from 'react-icons/go'
-import google from './../../assets/g.png'
-import microsoft from './../../assets/m.webp'
-import amazone from './../../assets/a.jpg'
-import apple from './../../assets/apple.jpg'
-import twitter from './../../assets/t.jpg'
-import meta from './../../assets/meta.webp'
+import { FaRegHeart } from 'react-icons/fa';
+import { GoLocation } from 'react-icons/go';
+import google from './../../assets/g.png';
+import microsoft from './../../assets/m.webp';
+import amazone from './../../assets/a.jpg';
+import apple from './../../assets/apple.jpg';
+import twitter from './../../assets/t.jpg';
+import meta from './../../assets/meta.webp';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 const Home = () => {
     const navigate = useNavigate();
     const apply = () => {
         navigate('/login');
     }
+
     return (
         <div>
             <div
@@ -26,8 +28,24 @@ const Home = () => {
                     backgroundSize: 'cover',
                 }}>
                 <div className='mt-20 text-6xl font-sans font-bold'>
-                    <h1 className='mb-2'>Find world’s</h1>
-                    <h1 className='text-5xl'>
+                    <motion.h1 className='mb-2'
+                        initial={{ x: -200 }}
+                        animate={{ x: 0 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 130,
+                            damping: 13,
+                        }}
+                    >Find world’s</motion.h1>
+                    <motion.h1 className='text-5xl'
+                        initial={{ x: -200 }}
+                        animate={{ x: 0 }}
+                        transition={{
+                            delay: 0.1,
+                            type: "spring",
+                            stiffness: 130,
+                            damping: 13,
+                        }}>
                         Best <ReactTypingEffect
                             text={["Job", "Remote Job", "Offer"]}
                             eraseDelay={2000}
@@ -37,28 +55,69 @@ const Home = () => {
                             cursor={' '}
                             className={'text-primary'}
                         />
-                    </h1>
-                    <p className='text-lg mt-4 font-normal'>
+                    </motion.h1>
+                    <motion.p className='text-lg mt-4 font-normal'
+                        initial={{ x: -200 }}
+                        animate={{ x: 0 }}
+                        transition={{
+                            delay: 0.2,
+                            type: "spring",
+                            stiffness: 130,
+                            damping: 13,
+                        }}>
                         Discover the best remote and work from <br /> home jobs at top remote companies.
-                    </p>
+                    </motion.p>
                 </div>
 
-                <div className='mt-10 font-sans flex gap-5'>
+                <motion.div className='mt-10 font-sans flex gap-5'
+                    initial={{ x: -200 }}
+                    animate={{ x: 0 }}
+                    transition={{
+                        delay: 0.3,
+                        type: "spring",
+                        stiffness: 130,
+                        damping: 13,
+                    }}>
                     <div className='text-center border-r border-gray-400 pr-5'>
                         <h1 className='font-bold text-primary text-3xl'>60K+</h1>
-                        <h3 className='text-gray-500'>Jobs submited</h3>
+                        <h3 className='text-black-200'>Jobs submited</h3>
                     </div>
                     <div className='text-center'>
                         <h1 className='font-bold text-primary text-3xl'>30K+</h1>
-                        <h3 className='text-gray-500'>Monthly Users</h3>
+                        <h3 className='text-black-200'>Monthly Users</h3>
                     </div>
-                </div>
+                </motion.div>
             </div>
             {/* Fresher Jobs */}
-            <div className='max-w-7xl mx-auto py-28 text-center'>
-                <h1 className='font-bold text-4xl mb-10'>Fresher Jobs</h1>
-                <div className='grid grid-cols-2 gap-5'>
-                    <div className='border px-8 py-6 rounded-xl'>
+            <div className='max-w-7xl mx-auto py-28 text-center overflow-hidden'>
+                <motion.h1 className='font-bold text-4xl mb-10'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 180,
+                        damping: 13,
+                        duration: 0.3
+                    }}
+                    variants={{
+                        visible: { y: 0 },
+                        hidden: { y: 200 }
+                    }}>Fresher Jobs</motion.h1>
+                <div className='grid grid-cols-1 lg:grid-cols-2 md:px-10 gap-5'>
+                    <motion.div className='border px-8 py-6 rounded-xl'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 180,
+                        damping: 15,
+                    }}
+                    variants={{
+                        visible: { y: 0 },
+                        hidden: { y: 200 }
+                    }}>
                         <div className='flex gap-4'>
                             <div className='w-14'>
                                 <img className='w-full' src="https://civi.uxper.co/wp-content/uploads/2022/11/avatar_uxper.png" alt="" />
@@ -83,10 +142,22 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
-                    </div>
-                    <div className='border px-8 py-6 rounded-xl'>
+                    </motion.div>
+                    <motion.div className='border px-8 py-6 rounded-xl'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 180,
+                        damping: 15,
+                    }}
+                    variants={{
+                        visible: { y: 0 },
+                        hidden: { y: 200 }
+                    }}>
                         <div className='flex gap-4'>
                             <div className='w-14'>
                                 <img className='w-full rounded-full' src="https://civi.uxper.co/wp-content/uploads/2022/10/netomi-logo.webp" alt="" />
@@ -111,10 +182,24 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
-                    </div>
-                    <div className='border px-8 py-6 rounded-xl'>
+                    </motion.div>
+                    <motion.div className='border px-8 py-6 rounded-xl'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{
+                        delay: 0.1,
+                        type: "spring",
+                        stiffness: 180,
+                        damping: 15,
+                    }}
+                    variants={{
+                        visible: { y: 0 },
+                        hidden: { y: 200 }
+                    }}
+                    >
                         <div className='flex gap-4'>
                             <div className='w-14'>
                                 <img className='w-full rounded-full' src="https://civi.uxper.co/wp-content/uploads/2022/10/superside-logo.webp" alt="" />
@@ -139,10 +224,23 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
-                    </div>
-                    <div className='border px-8 py-6 rounded-xl'>
+                    </motion.div>
+                    <motion.div className='border px-8 py-6 rounded-xl'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{
+                        delay: 0.1,
+                        type: "spring",
+                        stiffness: 180,
+                        damping: 15,
+                    }}
+                    variants={{
+                        visible: { y: 0 },
+                        hidden: { y: 200 }
+                    }}>
                         <div className='flex gap-4'>
                             <div className='w-14'>
                                 <img className='w-full rounded-full' src="https://civi.uxper.co/wp-content/uploads/2022/10/10up-logo.webp" alt="" />
@@ -167,10 +265,23 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
-                    </div>
-                    <div className='border px-8 py-6 rounded-xl'>
+                    </motion.div>
+                    <motion.div className='border px-8 py-6 rounded-xl'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{
+                        delay: 0.2,
+                        type: "spring",
+                        stiffness: 180,
+                        damping: 15,
+                    }}
+                    variants={{
+                        visible: { y: 0 },
+                        hidden: { y: 200 }
+                    }}>
                         <div className='flex gap-4'>
                             <div className='w-14'>
                                 <img className='w-full rounded-full' src="https://civi.uxper.co/wp-content/uploads/2022/10/10up-logo.webp" alt="" />
@@ -195,10 +306,23 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
-                    </div>
-                    <div className='border px-8 py-6 rounded-xl'>
+                    </motion.div>
+                    <motion.div className='border px-8 py-6 rounded-xl'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{
+                        delay: 0.2,
+                        type: "spring",
+                        stiffness: 180,
+                        damping: 15,
+                    }}
+                    variants={{
+                        visible: { y: 0 },
+                        hidden: { y: 200 }
+                    }}>
                         <div className='flex gap-4'>
                             <div className='w-14'>
                                 <img className='w-full' src="https://civi.uxper.co/wp-content/uploads/2022/11/avatar_uxper.png" alt="" />
@@ -223,17 +347,17 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
-                    </div>
+                    </motion.div>
 
                 </div>
             </div>
-            
+
             {/* Experience Jobs */}
             <div className='max-w-7xl mx-auto pb-28 text-center'>
                 <h1 className='font-bold text-4xl mb-10'>Experienced Jobs</h1>
-                <div className='grid grid-cols-2 gap-5'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 md:px-10 gap-5'>
                     <div className='border px-8 py-6 rounded-xl'>
                         <div className='flex gap-4'>
                             <div className='w-14'>
@@ -259,7 +383,7 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
                     </div>
                     <div className='border px-8 py-6 rounded-xl'>
@@ -287,7 +411,7 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
                     </div>
                     <div className='border px-8 py-6 rounded-xl'>
@@ -315,7 +439,7 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
                     </div>
                     <div className='border px-8 py-6 rounded-xl'>
@@ -343,7 +467,7 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
                     </div>
                     <div className='border px-8 py-6 rounded-xl'>
@@ -371,7 +495,7 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
                     </div>
                     <div className='border px-8 py-6 rounded-xl'>
@@ -399,7 +523,7 @@ const Home = () => {
                                     <span className='text-primary text-xs'>$100 - $200/month</span>
                                 </div>
                             </div>
-                            <button onClick={()=>apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
+                            <button onClick={() => apply()} className='btn btn-sm rounded-full btn-primary'>Apply Now</button>
                         </div>
                     </div>
 
@@ -408,7 +532,7 @@ const Home = () => {
             {/* Top IT Companies */}
             <div className='max-w-7xl mx-auto mb-20'>
                 <h1 className='font-bold text-4xl mb-10 text-center'>Top IT Companies</h1>
-                <div className='grid grid-cols-3 gap-7'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:px-10 gap-7'>
                     <div className='border p-5 rounded-lg'>
                         <img className='w-full' src="https://res.cloudinary.com/dteeny3pa/image/upload/v1676550964/kaj_qcq2kr.png" alt="" />
                         <a href="https://kaz.com.bd/" className='btn btn-primary mt-5 w-full'>View Details</a>
